706a6d6fb94c65feab5b7eca9c1d26c6c512146b
Dee Palmer <dpalmer.it@gmail.com>
2026-05-21 23:06:00 -0400

    fix: force dynamic on lab layout to prevent NextAuth prerender errors

--- app/lab/layout.tsx
+++ app/lab/layout.tsx
@@ -1,5 +1,7 @@
 import SessionWrapper from '@/app/components/lab/SessionWrapper'
 
+export const dynamic = 'force-dynamic'
+
 export default function LabLayout({ children }: { children: React.ReactNode }) {
   return <SessionWrapper>{children}</SessionWrapper>
 }
